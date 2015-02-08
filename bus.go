14849fa16743c5dee0a5098c67f1c6e3767202cc
Victor Peinado <vitojph@gmail.com>
2015-02-08 17:17:42 +0100

    update timetables for 528 and filter results

--- bus.go
+++ bus.go
@@ -2,67 +2,108 @@ package main
 
 import (
     "fmt"
+    "os"
+    "bufio"
+    "strings"
     "time"
-    )
+    "strconv"
+)
 
-
-// creamos un struct Servicio con la información relativa a cada servicio de autobus
-type Servicio struct {
-    linea int
-    destino string
-    salida int
-    llegada string
-    recorrido string
-}
-
-// Servicios es un slice de elementos Servicio
-type Servicios []Servicio
-
-type myTime struct {
-    time.Time // anonymous field
-}
-
-// Carga un listado de horarios de una línea
-func cargaLinea() Servicios {
-
-    var servicios[]Servicio
-
-    for i := 0; i < 24; i++ {
-        servicio := Servicio{linea:528, destino:"Madrid", salida:i, recorrido:"Cuesta del Águila"}
-        servicios = append(servicios, servicio)
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
     }
-    return servicios
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
 }
+
+// Reading files requires checking most calls for errors.
+// This helper will streamline our error checks below.
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 
 // filtra los servicios dependiendo de la hora
-func filtraServicios() Servicios  {
+func filtraServicios(servicios Servicios) Servicios  {
     // TODO: añadir destino como argumento
-    // carga los horarios
-    servicios := cargaLinea()
+    // y número total de expediciones
     var proximosServicios[]Servicio
     now := time.Now()
     // recorremos la lista de servicios e imprimimos los próximos
     for _, servicio := range servicios {
-        if servicio.salida >= now.Hour() {
-            proximosServicios = append(proximosServicios, servicio)
+        if servicio.h_salida >= now.Hour() {
+            if servicio.m_salida >= now.Minute(){
+                proximosServicios = append(proximosServicios, servicio)
+            }
         }
     }
     return proximosServicios
 }
 
+// creamos un struct Servicio con la información relativa a cada servicio de autobus
+type Servicio struct {
+    horario string
+    linea int
+    destino string
+    h_salida int
+    m_salida int
+}
+
+// Servicios es un slice de elementos Servicio
+type Servicios []Servicio
+
+
 func main() {
+    // creo el slice de servicios
+    var servicios[]Servicio
+
+    // cargo los horarios
+    lines, err := readLines("/data/go/src/github.com/vitojph/bus/528.data")
+    check(err)
+    for _, line := range lines {
+        if !strings.HasPrefix(line, "//") {
+            fields := strings.Split(line, ",")
+            linea, err := strconv.Atoi(fields[1])
+            check(err)
+            h_salida, err := strconv.Atoi(fields[3])
+            check(err)
+            m_salida, err := strconv.Atoi(fields[4])
+            check(err)
+
+            esteServicio := Servicio{horario: fields[0], linea: linea, destino: fields[2], h_salida: h_salida, m_salida: m_salida}
+            servicios = append(servicios, esteServicio)
+        }
+    }
 
     p := fmt.Printf
     now := time.Now()
     p("Ahora es %s\n", now)
 
-    //t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
-    //p("Go launched at %s\n", t.Local())
+    proximosBuses := filtraServicios(servicios)
+    p("Navalcarnero -> Madrid\n")
+    for _, item := range proximosBuses{
+        if item.destino == "madrid" {
+            fmt.Println(item)
+        }
+    }
 
-    //salida := time.Date(2015, 1, 1, 9, 0, 0, 0, time.UTC)
-    //p("La hora que he creado es %s\n", salida)
-
-    proximosBuses := filtraServicios()
-    fmt.Println(proximosBuses)
+    p("\nMadrid -> Navalcarnero\n")
+    for _, item := range proximosBuses{
+        if item.destino == "navalcarnero" {
+            fmt.Println(item)
+        }
+    }
 
 }
